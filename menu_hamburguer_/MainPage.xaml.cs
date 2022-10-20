c6d11ce4e13182802510e27654a84feaa73b7038
julyo hidalgo <julyohidalgo0@gmai.com>
2022-10-20 15:56:11 -0300

    atualizando

--- menu_hamburguer_/MainPage.xaml.cs
+++ menu_hamburguer_/MainPage.xaml.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace menu_hamburguer_
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
         }
     }
 }
